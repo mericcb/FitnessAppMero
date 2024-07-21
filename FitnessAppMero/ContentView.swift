7ca165dbd4b4643e442037c7f54e72de901f0951
Meriç Cem Baysar <mericcembaysar2002@hotmail.com>
2024-07-21 15:13:25 +0300

    initial commit

---
+++ FitnessAppMero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitnessAppMero
+//
+//  Created by Meriç Cem Baysar on 21.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitnessAppMero/FitnessAppMeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppMeroApp.swift
+//  FitnessAppMero
+//
+//  Created by Meriç Cem Baysar on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppMeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
